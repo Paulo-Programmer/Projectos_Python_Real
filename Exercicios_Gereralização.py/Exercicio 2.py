8330a753503977bf97c45446ae27bfad2276131d
Paulo_python <paulodaniel.psd@gmail.com>
2025-03-04 23:11:56 -0300

    commit ex2
    Realizei alterações e completo código com algumas funções.

--- Exercicios_Gereralização.py/Exercicio 2.py
+++ Exercicios_Gereralização.py/Exercicio 2.py
@@ -9,17 +9,34 @@ class Veiculo:
         self.ano = ano
     
     def ligar(self):
-        ...
+        print(f'{self.marca} {self.modelo} está ligado.')
 
     def desligar(self):
-        ...
+        print(f'{self.marca} {self.modelo} está desligado.')
 
 class Carro(Veiculo):
-    ...
+    
+    def abrir_portas(self):
+        print(f'As portas do {self.marca} {self.modelo}')
 
 class Moto(Veiculo):
-    ...
+    def empinar(self):
+        print(f'A moto {self.marca} {self.modelo} está empinando!')
+    
 
 class Caminhao(Veiculo):
-    ...
-    
+    def carregar_carga(self):
+        print(f'O caminhão {self.marca} {self.modelo} está sendo carregado.')
+
+carro = Carro('Toyota', 'Corolla', 2023)
+carro.ligar()
+carro.abrir_portas()
+
+moto = Moto('Honda', 'CB 500', 2022)
+moto.ligar()
+moto.empinar()
+
+
+caminhao = Caminhao('Volvo', 'FH 540', 2021)
+caminhao.ligar()
+caminhao.carregar_carga()
